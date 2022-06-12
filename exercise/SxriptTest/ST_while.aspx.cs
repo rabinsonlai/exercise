4815dad315379ae92a3cceca6e85339648efd833
unknown <rabinsonlai@gmail.com>
2022-06-12 12:16:33 +0800

    add sc

---
+++ exercise/SxriptTest/ST_while.aspx.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace exercise.SxriptTest
+{
+    public partial class ST_while : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
